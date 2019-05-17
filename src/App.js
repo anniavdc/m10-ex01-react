52b505641806fffb1248c0ed12c16b5b86a775a4
anniavdc <anva1189@gmail.com>
2019-05-17 19:12:57 +0200

    Update gender state

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      people: []
+      people: [],
+      filters: {
+        genres: [],
+        cities: []
+      }
     };
     this.fetch();
+    this.handleClick=this.handleClick.bind(this);
   }
   fetch() {
     fetch("https://randomuser.me/api/?results=50")
@@ -17,11 +22,43 @@ class App extends React.Component {
         this.setState({ people: data.results });
       });
   }
+  handleClick(event) {
+    const inputGender= event.currentTarget.checked;
+    if (inputGender){
+      this.setState(prevState => {
+        return {
+          filters:{
+            ...prevState.filters,
+            genres: prevState.filters.genres.concat('female')
+          }
+        }    
+      });
+    }else {
+      this.setState(prevState => {
+        return {
+          filters:{
+            ...prevState.filters,
+            genres: prevState.filters.genres.filter(item=> item !== 'female')
+          }
+        }    
+      });
+    }
+  }
   render() {
     const people = this.state.people;
 
     return (
       <div className="App">
+        <input
+          type="checkbox"
+          name="gender"
+          id="female"
+          value="female"
+          onClick={this.handleClick}
+        />
+        <label htmlFor="female">Female</label>
+        <input type="checkbox" name="gender" id="male" value="male" />
+        <label htmlFor="male">Male</label>
         <CardList people={people} />
       </div>
     );
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -9,6 +9,8 @@ class CardList extends React.Component {
           <li key={person.login.uuid}>
             <img src={person.picture.medium} alt="person" />
             <h1>{`${person.name.first} ${person.name.last}`}</h1>
+            <p>{person.gender}</p>
+            <p>{person.location.city}</p>
           </li>
         ))}
       </ul>
